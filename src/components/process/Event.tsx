3c1c1dcfc18a2b7027ba3e08133d8e016f17ec92
Ilin Vladislav <ayhalrayz@gmail.com>
2022-07-18 16:18:50 +0300

    feat: unique name and pay for card

--- src/components/process/Event.tsx
+++ src/components/process/Event.tsx
@@ -12,31 +12,31 @@ const Event: FC = observer(() => {
     const [isRoll, setRoll] = useState(true);
     const [isBuy, setBuy] = useState(false)
 
+    function typeEventTrget() {
+    const player = playerState.players.find(el => el.name === playerState.playerName);
+        if(player && fieldState.performance) {
+            const card = fieldState.performance.border[player.position];
+            if (card.type === 'COMPANY' && !fieldState.cardStates[card.id].ownerName) {
+                setRoll(false);
+                setBuy(true)
+            } else if (card.type === 'COMPANY' && fieldState.cardStates[card.id].ownerName !== player.name) {
+                console.log(1111)
+            }
+        }
+    }
+
     useEffect(() => {
         if(sessionState.moveStatus === MoveStatus.Start) {
             setRoll(true);
         } else if(sessionState.moveStatus === MoveStatus.Middle) {
-            const player = playerState.players.find(el => el.name === playerState.playerName);
-            if(player && fieldState.performance) {
-                const card = fieldState.performance.border[player.position];
-                if (card.type === 'COMPANY' && !fieldState.cardStates[card.id].ownerName) {
-                    setRoll(false);
-                    setBuy(true);
-                }
-            }
+            typeEventTrget()
         }
     }, [])
 
     function setShowRoll() {
         setRoll(!isRoll);
         setTimeout(() => {
-            const player = playerState.players.find(el => el.name === playerState.playerName);
-            if(player && fieldState.performance) {
-                const card = fieldState.performance.border[player.position];
-                if(card.type === 'COMPANY' && !fieldState.cardStates[card.id].ownerName) {
-                    setShowBuy()
-                }
-            }
+            typeEventTrget()
         }, 600)
     }
 
