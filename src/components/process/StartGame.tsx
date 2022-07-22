e4a05339f3cfeb6f6539b4509c4655554c9c45fc
Ilin Vladislav <ayhalrayz@gmail.com>
2022-07-22 14:12:46 +0300

    fix: start game

--- src/components/process/StartGame.tsx
+++ src/components/process/StartGame.tsx
@@ -1,26 +1,29 @@
 import React, {FC, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import './process.scss'
-import { sendNewMoveStatus, sendStartGame } from '../../backend';
+import { sendStartGame } from '../../backend';
 import sessionState from '../../store/SessionState';
 import playerState from '../../store/PlayerState';
 import { MoveStatus } from '../../enum';
+import { observer } from 'mobx-react-lite';
 
 
-const StartGame: FC = () => {
+const StartGame: FC = observer(() => {
     function start() {
         if(playerState.playerName) {
-            // sendNewMoveStatus(sessionState.sessionId);
             sessionState.moveStatus = MoveStatus.Start;
-            console.log(sessionState.moveStatus)
             sendStartGame(sessionState.sessionId, playerState.playerName);
         }
+    }
+
+    function isAcсess() {
+        return playerState.players.length <= 1
     }
     return <div className='process'>
         <div className="process__header">Начать игру</div>
         <div className="process__body">После начала никто не сможет присоедениться</div>
-        <Button style={{fontSize: '10px'}} onClick={start}>Начать</Button>
+        <Button style={{fontSize: '10px'}} onClick={start} disabled={isAcсess()}>Начать</Button>
     </div>
-}
+})
 
 export default StartGame
