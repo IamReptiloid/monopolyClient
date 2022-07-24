c10b4cc8b0e1325be263019fa16607f4056f41d6
Ilin Vladislav <ayhalrayz@gmail.com>
2022-07-24 13:59:54 +0300

    fix: deploy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,19 @@
-import '../style/index.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../components/process/dice.scss'
-import '../components/process/process.scss'
-import '../components/player/addPlayer.scss'
-import '../components/player/playerCards.scss'
-import '../components/player/playerCard.scss'
-import '../components/player/token.scss'
-import '../components/field/cardInfo.scss'
-import '../components/field/field.scss'
-import '../components/field/rectangularCard.scss'
-import '../components/field/squareCard.scss'
-import '../components/chat/chat.scss'
+import '../style/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/process/dice.scss';
+import '../components/process/process.scss';
+import '../components/player/addPlayer.scss';;
+import '../components/player/playerCards.scss';
+import '../components/player/playerCard.scss';
+import '../components/player/token.scss';
+import '../components/field/cardInfo.scss';
+import '../components/field/field.scss';
+import '../components/field/rectangularCard.scss';
+import '../components/field/squareCard.scss';
+import '../components/chat/chat.scss';
 import 'simplebar-react/dist/simplebar.min.css';
-import '../components/Monopoly.module.scss'
-import '../components/Registration.module.scss'
+import '../components/monopoly.scss';
+import '../components/registration.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
